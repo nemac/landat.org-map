0fb14cbebec9152e493138eb523ffc95cf562bc6
John Frimmel <jrfrimme@unca.edu>
2017-02-20 15:01:27 -0500

    adds polar plots

--- map.js
+++ map.js
@@ -44,7 +44,7 @@ function getData(lat, lng, div) {
         var response = this.responseText;
         response = response.replace(/\[|\]|\'/g, "").split(", ");
         drawGraph(response, div);
-        console.log(response)
+//        console.log(response)
     });
     oReq.open("GET", url);
     oReq.send()
@@ -55,7 +55,8 @@ function drawGraph(data, div) {
     var reprocessedData = reprocessData(data);
     makeUpDownLineGraph(data, div, reprocessedData["medians"]);
     makeUpDownOverlapingLineGraphWithCheckboxes(reprocessedData, div);
-    console.log(reprocessedData);
+    drawUpDownPolarWithCheckboxesAndThresholds(reprocessedData, div);
+//    console.log(reprocessedData);
     var list = document.getElementById("graph-list");
     list.appendChild(div);
 }
@@ -197,17 +198,393 @@ function makeUpDownOverlapingLineGraphWithCheckboxes (data, div) {
     var inputwrapper = wrapper.append("div").classed("input-wrapper", true);
 
     data.keys.forEach(function (key) {
-        console.log(key)
         createCheckbox(inputwrapper, key, "timeseries", year, charts, data, valueline, svg, averages);
     });
 
     createCheckbox(inputwrapper, "medians", "timeseries", "medians", charts, data, valueline, svg, averages);
 }
 
+function findPolarCenter (data) {
+    var i, j, length, arr;
+    var totalSum = 0;
+    var incompleteYears = 0;
+    var sum;
+    length = 46;
+
+    for (i = 0; i < data.keys.length; i++) {
+        arr = data[data.keys[i]];
+        if (arr.length !== length) {
+            incompleteYears++;
+            continue;
+        }
+        sum = 0;
+        for (j = 0; j < length/2; j++) {
+            sum += (arr[j][1] - arr[j+23][1]);
+        }
+        sum = sum / 23;
+        totalSum += sum;
+    }
+    totalSum = Math.abs(totalSum) / (data.keys.length - incompleteYears);
+
+    var areaDiff = 1000000;
+    var checkDiff;
+    var areaIndex = 0;
+    var leftArea, rightArea;
+    var avgs = data.medians;
+    var k, counter;
+
+    for (i = 0; i < length/2; i++) {
+        leftArea = 0;
+        rightArea = 0;
+        for (counter = 0; counter < length/2; counter++) {
+            j = (i + counter) % 46;
+            k = (j + 23) % 46;
+
+            leftArea += parseInt(avgs[j], 10);
+            rightArea += parseInt(avgs[k],10);
+        }
+        checkDiff = Math.abs(leftArea - rightArea);
+        if (checkDiff < areaDiff) {
+            console.log("hey")
+            areaDiff = checkDiff;
+            areaIndex = i;
+        }
+    }
+
+    var firstRadius = parseInt(avgs[areaIndex], 10);
+    var secondRadius = parseInt(-avgs[areaIndex + 23], 10);
+
+    var firstDiff = Math.abs(totalSum - firstRadius) - Math.abs(totalSum - secondRadius);
+    var secondDiff = Math.abs(-totalSum - firstRadius) - Math.abs(-totalSum - secondRadius);
+    if (secondDiff > firstDiff) {
+        areaIndex = areaIndex + 23;
+    }
+
+    var circlecenter = [0, 0];
+    var datacenter = [(areaIndex * 8) + 3, totalSum];
+
+    return([circlecenter, datacenter]);
+}
+
+/**
+ * startDay is actually the seasonality index, if it occurs after july it should be flipped
+ */
+function findPolarThresholds (data, startDay) {
+    var startIndex = Math.floor((startDay - 3) / 8);
+    if (startIndex > 22) {
+        startIndex = startIndex - 23;
+    }
+
+    var i, j, length, arr;
+    var totalSum = 0;
+    var sum;
+    length = 46;
+
+    for (i = 0; i < length; i++) {
+        j = (startIndex + i) % length;
+        totalSum += parseInt(data[j], 10);
+    }
+
+    var fifteenThreshold = totalSum * .15;
+    var eightyThreshold = totalSum * .80;
+    var eightyfiveThreshold = totalSum * .85;
+    var fifteenIndexFound = false, 
+        eightyIndexFound = false,
+        eightyfiveIndexFound = false;
+    var fifteenIndex, eightyIndex, eightyfiveIndex;
+
+    totalSum = 0;
+    for (i = 0; i < length; i++) {
+        j = (startIndex + i) % length;
+        totalSum += parseInt(data[j], 10);
+        if (!fifteenIndexFound && totalSum > fifteenThreshold) {
+            fifteenIndex = j;
+            fifteenIndexFound = true;
+            continue;
+        }
+        if (!eightyIndexFound && totalSum > eightyThreshold) {
+            eightyIndex = j;
+            eightyIndexFound = true;
+            continue;
+        }
+        if (!eightyfiveIndexFound && totalSum > eightyfiveThreshold) {
+            eightyfiveIndex = j;
+            eightyfiveIndexFound = true;
+            break;
+        }
+    }
+
+    var circleCenter = [0, 0];
+
+    var fifteenEnd = [(fifteenIndex * 8) + 3, 100];
+    var eightyEnd = [(eightyIndex * 8) + 3, 100];
+    var eightyfiveEnd = [(eightyfiveIndex * 8) + 3, 100];
+
+    return [
+        {
+            "label" : "15%",
+            "data" : [circleCenter, fifteenEnd]
+        },
+        {
+            "label" : "80%",
+            "data" : [circleCenter, eightyEnd]
+        },
+        {
+            "label" : "85%",
+            "data" : [circleCenter, eightyfiveEnd]
+        }
+    ];
+}
+
+function drawUpDownPolarWithCheckboxesAndThresholds (data, div) {
+    var year = "2015";
+    var width = 490,
+        height = 490,
+        radius = Math.min(width, height) / 2 - 30;
+
+    var averages = data["medians"]
+    var center = findPolarCenter(data);
+    var thresholds = findPolarThresholds(averages, center[1][0])
+
+    /**
+     * Sets up scaling of data. We know that the ndvi values fall between
+     * 0 & 100 so we set our domain to that. The range controls where the
+     * points will lie in our graph, so we set them to be between 0 and the
+     * radius.
+     */
+    var r = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, radius]);
+
+    /**
+     * function which will draw each point. To compute the distance from the
+     * center each point is we pass the datapoint to the function defined above.
+     * To determine the angle from the origin we need to convert the day to
+     * radians, so we convert the day to a number between 0 & 1 and then multiply
+     * it by 2 pi.
+     */
+    var line = d3.radialLine()
+        .radius(function(d) { return Array.isArray(d) ? r(d[1]) : r(d); })
+        .angle(function(d, i) {
+            var day = Array.isArray(d) ? parseJulianDay(d[0]) : (i * 8) + 3;
+            return ((((day - 1)%365)/365) * (2*Math.PI));
+        });
+
+    /**
+     * Sets up the canvas where the circle will be drawn.
+     */
+    var wrapper = d3.select(div).append("div").classed("polar-graph", true);
+    var svg = wrapper.append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    svg.call(tip);
+
+    /**
+     * This block of code draws the big circles of the graph & their labels
+     */
+    var gr = svg.append("g")
+        .attr("class", "r axis")
+        .selectAll("g")
+        .data(r.ticks(5).slice(1))
+        .enter().append("g");
+
+    gr.append("circle")
+        .attr("r", r);
+
+    gr.append("text")
+        .attr("y", function(d) { return -r(d) - 4; })
+        .attr("transform", "rotate(15)")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d; });
+
+    /**
+     * This block of code draws the labels for each month and the lines
+     * that go out to them.
+     */
+    var ga = svg.append("g")
+        .attr("class", "a axis")
+        .selectAll("g")
+        .data(d3.range(0, 360, 30))
+        .enter().append("g")
+        .attr("transform", function(d) { return "rotate(" + (d - 90) + ")"; });
+
+    ga.append("line")
+        .attr("x2", radius);
+
+    ga.append("text")
+        .attr("x", radius + 6)
+        .attr("dy", ".35em")
+        .style("text-anchor", function(d) { return d < 360 && d > 180 ? "end" : null; })
+        .attr("transform", function(d) { return d < 360 && d > 180 ? "rotate(180 " + (radius + 6) + ",0)" : null; })
+        .text(function(d) { return MONTH_LABELS[d/30]; });
+
+    /**
+     * Draws the threshold lines
+     */
+    var thresholdElem = svg.append("g")
+        .selectAll("g")
+        .data(thresholds)
+        .enter().append("g")
+        .attr("transform", function(d) { return "rotate(" + (d.data[1][0] - 90) + ")"; });
+
+    thresholdElem.append("line")
+        .attr("class", "line")
+        .attr("x2", radius);
+
+    thresholdElem.append("text")
+        .attr("x", radius + 6)
+        .attr("y", function (d) { return ((((d.data[1][0] - 1)%365)/365) * (2*Math.PI)); })
+        .attr("dy", ".35em")
+        .style("text-anchor", function(d) { var day = d.data[1][0]; return day < 360 && day > 180 ? "end" : null; })
+        .attr("transform", function(d) { var day = d.data[1][0]; return day < 360 && day > 180 ? "rotate(180 " + (radius + 6) + ",0)" : null; })
+        .text(function(d) { return d.label; });
+
+    /**
+     * Draws the line to the center of the data
+     */
+    var centerDay = center[1][0];
+    var centerDayOpposite = (centerDay + (365/2)) % 365;
+    var centerDayData = [centerDay, 100];
+    var centerDayOppositeData = [centerDayOpposite, 100];
+    var growingSeasonData = [centerDayData, centerDayOppositeData]
+
+    drawPolarPath(growingSeasonData, line, svg)
+        .classed("growing-season-line", "true");
+
+    drawPolarPath(center, line, svg)
+        .classed("center-line", "true");
+
+    svg.selectAll("point")
+        .data([center[1]])
+        .enter()
+        .append("circle")
+        .attr("class", "point")
+        .attr("transform", function(d) {
+            var coors = line([d]).slice(1).slice(0, -1);
+            return "translate(" + coors + ")"
+        })
+        .attr("r", 2.5)
+        .attr("stroke", "#000")
+        .attr("fill", "#dd82d2")
+        .on("mouseover", function(d) {
+            tip.show("Center: "  + String(d[1]).substring(0, 7));
+            this.setAttribute("r", 5);
+            this.setAttribute("stroke-width", "2px");
+            d3.select(this).classed("active", true);
+        })
+        .on("mouseout", function (d) {
+            tip.hide();
+            this.setAttribute("r", 2.5);
+            this.setAttribute("stroke-width", "1px");
+            d3.select(this).classed("active", true);
+        });
+    
+    var charts = {};
+
+    /**
+     * This block of code draws the line that the data follows
+     */
+    charts["medians"] = {
+        "path" : drawPolarPath(averages, line, svg)
+    };
+
+    charts[year] = {
+        "path" : drawPolarPath(data[year], line, svg)
+    };
+
+    /**
+     * This block of code draws the point at each data point
+     */
+    charts.medians.points = drawLinearPoints(averages, line, svg, averages);
+    charts[year].points = drawLinearPoints(data[year], line, svg, averages);
+
+    var inputwrapper = wrapper.append("div").classed("input-wrapper", true);
+
+    data.keys.forEach(function (key) {
+        var checkboxWrapper = inputwrapper.append("div");
+
+        checkboxWrapper.append("input")
+            .attr("type", "checkbox")
+            .attr("id", "polar-threshold-" + key)
+            .attr("value", key)
+            .property("checked", (key === "2015") ? true : false)
+            .on("change", function (e) {
+                var newYear = this.value;
+                if (!this.checked) {
+                    charts[newYear].path.remove();
+                    charts[newYear].points.remove();
+                } else {
+                    if (!charts.hasOwnProperty(newYear)) {
+                        charts[newYear] = {};
+                    }
+
+                    charts[newYear].path = drawPolarPath(data[newYear], line, svg);
+                    charts[newYear].points = drawLinearPoints(data[newYear], line, svg, averages);
+                }
+            });
+
+        checkboxWrapper.append("label")
+            .text(key)
+            .attr("for", "polar-threshold-" + key);
+    });
+
+    var checkboxWrapper = inputwrapper.append("div");
+
+    checkboxWrapper.append("input")
+        .attr("type", "checkbox")
+        .attr("id", "polar-average-threshold")
+        .attr("value", "medians")
+        .property("checked", true)
+        .on("change", function (e) {
+            var newYear = this.value;
+            if (!this.checked) {
+                charts[newYear].path.remove();
+                charts[newYear].points.remove();
+            } else {
+                if (!charts.hasOwnProperty(newYear)) {
+                    charts[newYear] = {};
+                }
+
+                charts[newYear].path = drawPolarPath(data[newYear], line, svg)
+
+                charts[newYear].points = drawLinearPoints(data[newYear], line, svg, averages);
+            }
+        });
+
+    checkboxWrapper.append("label")
+        .text("Baseline")
+        .attr("for", "polar-average-threshold");
+
+    var thresholdCheckbox= inputwrapper.append("div")
+        .classed("threshold-checkbox", true);
+
+    thresholdCheckbox.append("input")
+        .attr("type", "checkbox")
+        .attr("id", "threshold-checkbox")
+        .property("checked", true)
+        .on("change", function (e) {
+            thresholdElem.style("opacity", (this.checked) ? 1 : 0);
+        });
+
+    thresholdCheckbox.append("label")
+        .text("Thresholds")
+        .attr("for", "threshold-checkbox");
+}
+
 function drawLinearPath(data, line, svg) {
     return svg.append("path")
         .attr("class", "line")
-        .attr("d", line(data))
+        .attr("d", line(data));
+}
+
+function drawPolarPath(data, line, svg) {
+    return svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
 }
 
 function drawLinearPoints(data, line, svg, averages) {
