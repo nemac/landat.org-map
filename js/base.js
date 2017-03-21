48e93ed06a7b160147978f36a2b5c336561bb27f
John Frimmel <jrfrimme@unca.edu>
2017-03-21 14:21:53 -0400

    moves graph code into its own file

--- js/base.js
+++ js/base.js
@@ -2,15 +2,18 @@ import ParseConfig from './parser';
 import CreateSearch from './search';
 import renderLayerList from './renderLayerList';
 import CreateBaseLayers from './baselayer';
+import {SetupGraphs, BindGraphEvents} from './graph'
 
 var Base = function (config) {
     ParseConfig(config, callback);
+    SetupGraphs();
 }
 
 var callback = function (data) {
     CreateBaseLayers(map, data.baselayers);
     renderLayerList(map, data.layers, data.layout);
     CreateSearch(map);
+    BindGraphEvents(map);
 }
 
 window.Base = Base;
