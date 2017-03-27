e7800e4d155349673f622b96b4ef050895597a25
John Frimmel <jrfrimme@unca.edu>
2017-03-27 17:13:27 -0400

    adds basic baselayer parsing to parser

--- js/baselayer.js
+++ js/baselayer.js
@@ -1,15 +1,19 @@
 export default function CreateBaseLayers (map, layerConfig) {
     layerConfig = layerConfig || 
         [{
+            "id" : "carto-light-default",
             "url" : "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
             "attribution" : '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
         }];
-
     var i;
     var baseLayer;
+    var config;
 
     for (i = 0; i< layerConfig.length; i++) {
-        baseLayer = createBaseLayer(layerConfig[i]);
+        config = layerConfig[i];
+
+        if (!config.active) continue;
+        baseLayer = createBaseLayer(config);
         baseLayer.addTo(map);
     }
 }
@@ -17,6 +21,10 @@ export default function CreateBaseLayers (map, layerConfig) {
 function createBaseLayer (layerConfig) {
     return L.tileLayer(
         layerConfig.url,
-        { attribution: layerConfig.attribution }
+        {
+            id: layerConfig.id,
+            type: "baselayer",
+            attribution: layerConfig.attribution
+        }
     );
 }
