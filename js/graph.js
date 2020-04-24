67336380acaa3792848681d2df686fb26f65f51f
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-04-24 10:35:31 -0400

    Pushing up some updates. Some things are still a WIP

--- js/graph.js
+++ js/graph.js
@@ -62,9 +62,9 @@ function getData(poi, div) {
 }
 
 function splitData(data) {
-    var i;
-    for (i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         data[i] = data[i].split(",");
+        data[i] = [parseDate(data[i][0]), data[i][1]] // parse all date strings into dates
     }
     return data;
 }
@@ -78,10 +78,11 @@ function reprocessData (origdata) {
     data["keys"] = [];
     for (i = 0; i < origdata.length; i++) {
         point = origdata[i];
-        key = point[0].substring(0,4);
+        key = point[0].getFullYear();
         // The last data point for a year's data rolls over to the next year
         // at 1/3, so adjust the key (year) accordingly for these cases
-        if (point[0].substring(4) === "0103" || point[0].substring(4) === "0102") {
+        if (String(point[0].getMonth()).concat(String(point[0].getDate())) === "03" || 
+            String(point[0].getMonth()).concat(String(point[0].getDate())) === "02") {
           key = String(parseInt(key)-1)
         }
 
@@ -134,8 +135,8 @@ function buildPhenologicalYearData (rawJsonData, calendarYearData) {
     // Find where the pheno year begins for one year and capture that value. It will be the same for all years
     for (let i = 0; i < calendarYearData[2000].length; i++) {
         if (phenoYearFound) break
-        let dateString = calendarYearData[2000][i][0]
-        if (getDayOfYear(dateString) >= startDayOfPhenoYear) {
+        let date = calendarYearData[2000][i][0]
+        if (date.getDOY() >= startDayOfPhenoYear) {
             phenoYearFound = true
             phenoYearBeginDayIndex = i
         }
@@ -274,7 +275,7 @@ function makeZoomToMapMarkerButton(poi) {
 }
 
 function drawGraph(data, div, poi) {
-    data = splitData(data)
+    data = splitData(data) // split data and parse all date strings into dates
     var reprocessedData = reprocessData(data)
     let phenoYearData = buildPhenologicalYearData(data, reprocessedData)
     drawAllYearsGraph(data, div)
@@ -289,22 +290,15 @@ function roundFloat(number, decimalPlaces) {
 
 function drawAllYearsGraph(data, div) {
     let ndviData = []
-    let parsedDates = []
+    let dates = []
     data.forEach(function (item) {
-        parsedDates.push(parseDate(item[0]))
+        dates.push(item[0])
         ndviData.push(item[1])
     })
-    let dataPlotly = [{
-        type: 'scatter',
-        mode: 'lines+markers',
-        y: ndviData,
-        customdata: parsedDates,
-        hovertemplate: "%{customdata|%B %d, %Y}: %{y:.1f}<extra></extra>"
-    }]
 
     let layout = {
         hovermode: 'closest',
-        margin: { l: 28, r: 30, t: 50, b: 35 },
+        margin: { l: 18, r: 30, t: 50, b: 35 },
         modebar: { orientation: 'v' },
         xaxis: {
             tickangle: '-45',
@@ -316,6 +310,31 @@ function drawAllYearsGraph(data, div) {
             range: [0, 100]
         }
     }
+
+    let dataPlotly = [{
+        type: 'scatter',
+        mode: 'lines+markers',
+        y: ndviData,
+        customdata: dates,
+        hovertemplate: "%{customdata|%B %d, %Y}: %{y:.1f}<extra></extra>",
+        line: {
+            //color: ["#23758e","#2c798d","#357e8c","#3e838b","#47878a","#518c89","#5a9088","#639587","#6c9a86","#759e85",
+            //        "#7ea384","#88a883","#91ac82","#9ab181","#a3b580","#acba7f","#b5bf7e","#bec37d","#c8c87c"],
+            //cmin: layout.xaxis.tickvals[0],
+            //cmax: layout.xaxis.tickvals[-1],
+            //autocolorscale: false,
+            colorscale: [
+                ['0', '#23758e'],
+                ['46', '#c8c87c']
+            ]
+        },
+        /*marker: {
+            color: ["#23758e","#2c798d","#357e8c","#3e838b","#47878a","#518c89","#5a9088","#639587","#6c9a86","#759e85",
+                    "#7ea384","#88a883","#91ac82","#9ab181","#a3b580","#acba7f","#b5bf7e","#bec37d","#c8c87c"],
+            cmin: layout.xaxis.tickvals[0],
+            cmax: layout.xaxis.tickvals[-1],
+        },*/
+    }]
 
     var wrapper = d3.select(div).append("div").classed("timeseries-graph", true)
     let config = {displaylogo: false, displayModeBar: true, modeBarButtonsToRemove: []}
@@ -351,13 +370,11 @@ function drawOverlappingYearsGraph(data, div) {
             x: 1.07,
         },
         xaxis: {
-            showgrid: false,
             tickangle: '0',
             tickvals: [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335],
                 ticktext: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
         },
         yaxis: {
-            showgrid: false,
             range: [0, 100]
         }
     }
@@ -372,9 +389,9 @@ function buildScatterTrace(data, traceName, color, visibility = 'legendonly',
     let traceObject = {'x': [], 'y': [], 'dateArray': []}
     data.forEach(function (item, index) {
         // last day in the dataset is the first day of next year so we add 365 to it so it plots correctly
-        index === 45 ? traceObject.x.push(getDayOfYear(item[0]) + 365) : traceObject.x.push(getDayOfYear(item[0]))
+        index === 45 ? traceObject.x.push(item[0].getDOY() + 365) : traceObject.x.push(item[0].getDOY())
         traceObject.y.push(parseInt(item[1], 10))
-        traceObject.dateArray.push(parseDate(item[0]))
+        traceObject.dateArray.push(item[0])
     })
     return [{
         type: 'scatter',
@@ -435,7 +452,8 @@ function drawPolarGraph(data, div, phenoYearData) {
                                                           "%{customdata|%B %d}<br>NDVI: %{r:.1f}<extra></extra>"))
     
     var config = {responsive: true, displaylogo: false, displayModeBar: true, modeBarButtons: modeBarButtons}
-    Plotly.newPlot(wrapper.node(), dataPlotly, getPlotlyLayout(), config)
+    let plotlyLayout = getPlotlyLayout()
+    Plotly.newPlot(wrapper.node(), dataPlotly, plotlyLayout, config)
     let traceObject = {}
 
     /* On a legend click event, find out what trace was clicked on and grab all of the r values for that trace.
@@ -497,8 +515,8 @@ function buildTrace(data, traceName, color, visibility = 'legendonly',
     let traceObject = {'r': [], 'theta': [], 'dateArray': []}
     data.forEach(function (item) {
         traceObject.r.push(parseInt(item[1], 10))
-        traceObject.theta.push(getDayOfYear(item[0]))
-        traceObject.dateArray.push(parseDate(item[0]))
+        traceObject.theta.push(convertDayOfYearToDegrees(item[0]))
+        traceObject.dateArray.push(item[0])
     })
     return [{
         type: 'scatterpolar',
@@ -557,7 +575,7 @@ function buildCenterLine(centerlineData, visibility = true) {
     ]
 }
 
-function getPlotlyLayout() {
+function getPlotlyLayout(upperRange = 100) {
     return {
         dragmode: false, // disables zoom on polar graph
         modebar: {
@@ -586,14 +604,16 @@ function getPlotlyLayout() {
             radialaxis: {
                 visible: true,
                 fixedrange: true,
-                angle: 180,
-                side: 'counterclockwise',
-                tickangle: -180,
+                angle: 90,
+                side: 'clockwise',
+                tickangle: 90,
+                tickvals: [0, 20, 40, 60, 80],
+                nticks: 5,
                 gridcolor: '#E2E2E2',
                 tickfont: {
                     color: '#444',
                 },
-                range: [0, 100]
+                range: [0, upperRange],
             },
             angularaxis: {
                 visible: true,
@@ -601,7 +621,7 @@ function getPlotlyLayout() {
                 type: "linear",
                 tickmode: "array",
                 showticklabels: true,
-                tickvals: [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335],
+                tickvals: dayOfYearToDegrees(firstOfMonthJulianDays),
                 ticktext: [
                     "Jan",
                     "Feb",
@@ -617,30 +637,46 @@ function getPlotlyLayout() {
                     "Dec"
                 ],
                 direction: "clockwise",
+                rotation: false,
                 period: 12
             }
         }
     }  
 }
 
-function getDayOfYear (dateString) {
-    let date = parseDate(dateString)
-    let start = new Date(date.getFullYear(), 0, 0)
-    let diff = date - start
-    let oneDay = 1000 * 60 * 60 * 24
-    let day = Math.floor(diff / oneDay)
-    return day
+const convertDayOfYearToDegrees = date => {
+    let dayOfYear = date.getDOY()
+    return ((dayOfYear - 1) * 360) / 364
+}
+
+const dayOfYearToDegrees = array => {
+    let newArray = array.map(x => ((x - 1 ) * 360) / 364)
+    return newArray
 }
 
 const modeBarButtons = [[
     {
-        name: 'Reset Plot Layout',
-        icon: Plotly.Icons.home,
-        click: function(gd) {
-            Plotly.relayout(gd, getPlotlyLayout())
+        name: 'Zoom In',
+        icon: Plotly.Icons.zoom_plus,
+        click: function(div) {
+            Plotly.relayout(div, getPlotlyLayout(div.layout.polar.radialaxis.range[1] - 10))
         }
-     },
-     "toImage"
+    },
+    {
+       name: 'Zoom Out',
+        icon: Plotly.Icons.zoom_minus,
+        click: function(div) {
+            Plotly.relayout(div, getPlotlyLayout(div.layout.polar.radialaxis.range[1] + 10))
+        }
+    },
+    {
+        name: 'Reset axes',
+        icon: Plotly.Icons.home,
+        click: function(div) {
+            Plotly.relayout(div, getPlotlyLayout())
+        }
+    },
+    "toImage"
 ]]
 
 const repeat = (a, n) => Array(n).fill(a).flat(1)
@@ -751,145 +787,6 @@ function findPolarThresholds (data, startDay) {
     return { fifteenEnd, eightyEnd }
 }
 
-//////////////////////// GRAPH HELPERS ///////////////////////////////////
-
-function drawLinearPath(data, line, svg) {
-    return svg.append("path")
-        .attr("class", "line")
-        .attr("d", line(data));
-}
-
-function drawPolarPath(data, line, svg) {
-    return svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
-}
-
-function drawLinearPoints(data, line, svg) {
-    return svg.selectAll("point")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("r", 2)
-        .attr("class", "point")
-        .attr("transform", function(d, i) {
-            var point = Array.isArray(d) ? d : [(i*8) + 3, d];
-            var coors = line([point]).slice(1).slice(0, -1);
-            return "translate(" + coors + ")"
-        })
-        //.attr("r", 3)
-        .attr("stroke", "#000")
-        .attr("fill",function(d,i){
-            var val = Array.isArray(d) ? d[0].substring(0, 4) : 0;
-            return pullDistinctColor(val)
-        })
-        .on("mouseover", handlePointMouseover)
-        .on("mouseout", handlePointMouseout);
-}
-
-function handlePointMouseover(d) {
-    var tipString = Array.isArray(d) ? formatDate(d[0]) + ": "  + d[1] : "Average: "  + d;
-    tip.show(tipString);
-    this.setAttribute("r", "4");
-}
-
-function handlePointMouseout(d) {
-    tip.hide();
-    this.setAttribute("r", "2");
-
-    var activeType = document.getElementsByClassName("graph-type-btn active")[0].getAttribute('data-type');
-
-    //send google analytics tool tip on graph do this on the mouseout so mouseover dose not do a lot of events....
-    // we miss a few but that is better than over counting.
-    ga('send', 'event', {
-      eventCategory: 'graph',
-      eventAction: 'hover',
-      eventLabel: activeType + ' tool tip',
-      nonInteraction: false
-    });
-}
-
-function createCheckbox(wrapper, key, type, poi, charts, data, line, svg, averages) {
-    var checkboxWrapper = wrapper.append("div");
-    var lat = poi.lat;
-    var lng = poi.lng;
-
-    checkboxWrapper.append("input")
-        .attr("type", "checkbox")
-        .attr("id", type + "-" + key + lat.toString().replace(".", "") + "-" + lng.toString().replace(".", ""))
-        .attr("data-link", key + lat.toString().replace(".", "") + "-" + lng.toString().replace(".", ""))
-        .attr("value", key)
-        .property("checked", (poi.plots.indexOf(key) !== -1) ? true : false)
-        .on("change", function (e) {
-            var newYear = this.value;
-            if (!this.checked) {
-                handleCheckboxDisable(charts, newYear, data);
-                removeKeyFromPOI(poi, key);
-                //send google analytics graph year click off
-                dispatchGraphCheckboxClick(newYear + ' ' + type + ' timeseries off');
-            } else {
-                handleCheckboxEnable(charts, newYear, data, line, svg, averages);
-                addKeyToPOI(poi, key);
-                //send google analytics graph year click on
-                dispatchGraphCheckboxClick(newYear + ' ' + type + ' timeseries on');
-            }
-            handleCheckboxSync(key + lat.toString().replace(".", "") + "-" + lng.toString().replace(".", ""), this.checked);
-            updateShareUrl();
-        });
-
-    checkboxWrapper.append("label")
-        .text(key !== "baseline" ? key : "Baseline")
-        .attr("for", type + "-" + key + lat.toString().replace(".", "") + "-" + lng.toString().replace(".", ""));
-
-    checkboxWrapper.append("div")
-        .style("background", pullDistinctColor(key !== "baseline" ? key : 0))
-        .classed("graph-pip-example", true);
-}
-
-function handleCheckboxDisable (charts, newYear, data) {
-    charts[newYear].path.remove();
-    charts[newYear].points.remove();
-}
-
-function handleCheckboxEnable (charts, newYear, data, line, svg) {
-    if (!charts.hasOwnProperty(newYear)) {
-        charts[newYear] = {};
-    }
-    charts[newYear].path = drawLinearPath(data[newYear], line, svg);
-    charts[newYear].points = drawLinearPoints(data[newYear], line, svg);
-}
-
-function handleCheckboxSync (key, checkedStatus, wrapper) {
-    d3.selectAll("input[data-link='" + key + "']").each(function (p, j) {
-        var elem = d3.select(this);
-        if (elem.property("checked") !== checkedStatus) {
-            elem.property("checked", checkedStatus);
-            elem.dispatch("change");
-        }
-    });
-}
-
-function removeKeyFromPOI (poi, key) {
-    var index = poi.plots.indexOf(key);
-    if (index === -1) return;
-    poi.plots.splice(index, 1);
-}
-
-function addKeyToPOI (poi, key) {
-    if (poi.plots.indexOf(key) !== -1) return;
-    poi.plots.push(key);
-}
-
-function dispatchGraphCheckboxClick (label) {
-    ga('send', 'event', {
-        eventCategory: 'graph',
-        eventAction: 'click',
-        eventLabel:  label,
-        nonInteraction: false
-    });
-}
-
 function pullDistinctColor (year) {
     var colorRamp = [
         "#23758e",
@@ -922,8 +819,6 @@ function pullDistinctColor (year) {
     return (year === 0) ? "#fff" : colorRamp[parseInt(year, 10) % colorRamp.length];
 }
 
-///////////////////////// DATE HELPERS /////////////////////////////////
-
 function parseDate (date) {
     date = date.toString();
     var year = date.substring(0, 4);
@@ -933,67 +828,4 @@ function parseDate (date) {
     return new Date(year, month, day);
 }
 
-function parseJulianDay (date) {
-    if (typeof(date) === "string") {
-        date = parseDate(date);
-        return date.getDOY();
-    } else {
-        return date;
-    }
-}
-
-function formatDate (date) {
-    if (date === "Average") { return date; }
-
-    date = parseDate(date);
-    return formatMonth(date.getMonth()) + " " + ordinal_suffix_of(date.getDate()) + ", " + date.getFullYear();
-}
-
-function formatMonth (month) {
-    return FORMATTED_MONTH_LABELS[month];
-}
-
-function ordinal_suffix_of(day) {
-    var j = day % 10,
-        k = day % 100;
-    if (j === 1 && k !== 11) {
-        return day + "st";
-    }
-    if (j === 2 && k !== 12) {
-        return day + "nd";
-    }
-    if (j === 3 && k !== 13) {
-        return day + "rd";
-    }
-    return day + "th";
-}
-
-var MONTH_LABELS = {
-    0: "Jan",
-    1: "Feb",
-    2: "Mar",
-    3: "Apr",
-    4: "May",
-    5: "Jun",
-    6: "Jul",
-    7: "Aug",
-    8: "Sep",
-    9: "Oct",
-    10: "Nov",
-    11: "Dec"
-};
-
-var FORMATTED_MONTH_LABELS = {
-    0: "Jan.",
-    1: "Feb.",
-    2: "Mar.",
-    3: "Apr.",
-    4: "May",
-    5: "Jun.",
-    6: "Jul.",
-    7: "Aug.",
-    8: "Sep.",
-    9: "Oct.",
-    10: "Nov.",
-    11: "Dec."
-};
+var firstOfMonthJulianDays = [ 1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
