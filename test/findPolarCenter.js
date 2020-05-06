347f519588657887386e6c1bc805b61f03c5cd3e
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-05-06 09:59:54 -0400

    running build process for production

--- test/findPolarCenter.js
+++ test/findPolarCenter.js
@@ -5,8 +5,8 @@ var thetaValues = [140.44, 148.35, 156.26, 164.18, 172.09, 180, 187.91, 195.82, 
                   219.56, 227.47, 235.38, 243.30, 251.21, 259.12, 267.03, 274.95, 282.86, 290.77]
 
 describe('#findPolarCenter()', function() {
-  context('with number arguments', function() {
-    it('should return sum of arguments', function() {
+  context('example test case', function() {
+    it('should return r and theta values', function() {
       expect(findPolarCenter(ndviValues, thetaValues)).to.eql([48.97855032660815, 216.8703740444932])
     })
   })
